6a8aacc32c1bd2d9a58db507fe4da2167c32e27d
siewla <siewlapang@gmail.com>
2020-05-16 17:18:17 +0800

    basic completed

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: false }));
+
 // =======================================
 //              DATABASE
 // =======================================
@@ -15,12 +21,24 @@ const bakedGoods = require('./models/bakedgoods.js');
 // =======================================
 // index route
 app.get('/bakedgoods', (req, res) => {
-    res.send(bakedGoods);
+    res.render('bakedgoods/index',{ bakedGoods:bakedGoods });
+    //res.send(bakedGoods);
+});
+
+// new route
+app.get('/bakedgoods/new', (req, res) => {
+    res.render('bakedgoods/new');
 });
 
 // show route
 app.get('/bakedgoods/:id', (req, res) => {
-    res.send(bakedGoods[req.params.id]);
+    res.render('bakedgoods/show', { bakedItem: bakedGoods[req.params.id] });
+});
+
+//post route
+app.post('/bakedgoods', (req,res) =>{
+    bakedGoods.push(req.body);
+    res.redirect('/bakedgoods');
 });
 
 // =======================================
